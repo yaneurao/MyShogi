c63f4bb567f27df621ba53baaeaf37ec5df19c26
yaneurao <yaneurao@gmail.com>
2018-05-16 14:35:54 +0900

    - docs/解説.txt 追加 - Positionに駒落ちかそうでないかを表現するフラグが必要 	- そもそも棋譜の管理クラスが必要 	- Model/Shogi/IO/追加 		- PlayManager.cs追加 		    /// 1局の対局棋譜を全般的に管理するクラス 			/// ・分岐棋譜をサポート 			/// ・着手時間をサポート 			/// ・対局相手の名前をサポート 			/// ・CSA/KIF/KI2/PSN形式での入出力をサポート 			/// ・千日手の管理、検出をサポート 		- となる予定..(あとで考える)

---
+++ MyShogi/Model/Shogi/IO/PlayManager.cs
@@ -0,0 +1,14 @@
+﻿namespace MyShogi.Model.Shogi.IO
+{
+    /// <summary>
+    /// 1局の対局棋譜を全般的に管理するクラス
+    /// ・分岐棋譜をサポート
+    /// ・着手時間をサポート
+    /// ・対局相手の名前をサポート
+    /// ・CSA/KIF/KI2/PSN形式での入出力をサポート
+    /// ・千日手の管理、検出をサポート
+    /// </summary>
+    public class PlayManager
+    {
+    }
+}
