16d9c80f1c8bc9135e59ef7b54dcff1e9b4c1451
yaneurao <yaneurao@gmail.com>
2018-07-04 00:08:47 +0900

    - EvalValue、符号を反転させた時に、先後逆から見た評価値になることを保証されるように修正。

--- MyShogi/Model/Shogi/Core/EvalValue.cs
+++ MyShogi/Model/Shogi/Core/EvalValue.cs
@@ -5,33 +5,38 @@ namespace MyShogi.Model.Shogi.Core
     /// <summary>
     /// 評価値として用いる値。
     /// 探索部を用意していないので、表示に使う程度であるが…。
+    /// 
+    /// MatePlus = -MatedMinus
+    /// Mate = -Mated
+    /// 
+    /// のように符号を反転させると先後入替えた側から見た評価値になることが保証されている。
     /// </summary>
     public enum EvalValue : Int32
     {
-        // この値は使わない。
-        Unknown = Int32.MaxValue,
-
         // "score mate+"を表現する手数不明の詰み。
         MatePlus = Int32.MaxValue - 1,
 
         // 現局面で(敵玉が)詰んでいる時の評価値
-        // N手詰めのときは、(ValueMate - N)
+        // N手詰めのときは、(Mate - N)
         Mate = Int32.MaxValue - 2,
 
         Zero = 0,
 
         // 現局面で(自玉が)詰んでいる時の評価値
-        // N手で詰まされるときは、(ValueMate + N)
-        Mated = Int32.MinValue + 2,
+        // N手で詰まされるときは、(Mate + N)
+        Mated = Int32.MinValue + 3,
 
         // "score mate-"を表現する手数不明の詰まされ。
-        MatedMinus = Int32.MinValue + 1,
+        MatedMinus = Int32.MinValue + 2,
 
         // この局面の評価値が存在しないことを意味する値
         // 形勢グラフなどには、この値のところは描画してはならない。
-        NoValue = Int32.MinValue,
-    }
+        NoValue = Int32.MinValue + 1,
 
+        // この値は使わない。
+        Unknown = Int32.MinValue,
+    }
+    
     /// <summary>
     /// ある評価値が、探索のupperbound(上界) , lowerbound(下界)の値であるかなどを表現する。
     /// </summary>
