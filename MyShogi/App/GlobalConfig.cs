6a0673ef93272a32a0567ea1a7d464d3a842dfda
yaneurao <yaneurao@gmail.com>
2018-08-07 09:40:21 +0900

    - マスターアップ終わったのでドキュメント類を整理

--- MyShogi/App/GlobalConfig.cs
+++ MyShogi/App/GlobalConfig.cs
@@ -14,8 +14,12 @@ namespace MyShogi.App
     {
         /// <summary>
         /// MyShogiのバージョン文字列。Aboutダイアログに表示される。
+        ///
+        /// 『将棋神やねうら王』(2018年8月末発売)のマスターアップ版[2018/08/06]は"1.0.0"
+        /// 『将棋神やねうら王』のUpdate1は、"1.1.0"を予定。
         /// </summary>
-        public static readonly string MYSHOGI_VERSION_STRING = "1.0.0"; // マスターアップ予定版は1.0.0
+        public static readonly string MYSHOGI_VERSION_STRING = "1.0.1";
+
 
         public GlobalConfig()
         {
