cd0a4607d7761959377e0cdd216bcd6d9c38d479
yaneurao <yaneurao@gmail.com>
2018-10-13 13:51:16 +0900

    - ToolStrip with Custom ToolTip   - メインメニューのボタン、ミニ盤面のボタンのToolStripの文字フォントも表示設定のフォントで設定したものが反映するようになった。

--- MyShogi/View/Win2D/Common/ToolStripEx.cs
+++ MyShogi/View/Win2D/Common/ToolStripEx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyShogi.Model.Common.Collections;
+using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MyShogi.View.Win2D
@@ -10,6 +12,10 @@ namespace MyShogi.View.Win2D
     /// cf.
     /// http://bbs.wankuma.com/index.cgi?mode=al2&namber=69912&KLOG=119
     /// https://blogs.msdn.microsoft.com/rickbrew/2006/01/09/how-to-enable-click-through-for-net-2-0-toolstrip-and-menustrip/
+    /// 
+    /// あと、標準のToolStripだとToolTipをカスタマイズできないのでToolTipを自前で持つ。
+    /// cf.
+    /// https://www.codeproject.com/Articles/376643/ToolStrip-with-Custom-ToolTip
     /// </summary>
     public partial class ToolStripEx : ToolStrip
     {
@@ -18,6 +24,8 @@ namespace MyShogi.View.Win2D
         /// 上の4つの定数から選ぶ。MA_NOACTIVATEがこのToolStripExのデフォルト。
         /// </summary>
         public ClickActionEnum ClickAction { get; set; } = ClickActionEnum.MA_NOACTIVATE;
+
+        // これMonoでもうまく動くのかどうかわからんが、たぶん動くのかな…。
 
         protected const int WM_MOUSEACTIVATE = 0x0021;
 
@@ -31,5 +39,114 @@ namespace MyShogi.View.Win2D
                 m.Result = (IntPtr)ClickAction;
             }
         }
+
+        // -- 以下、Custom Tooltipのための実装
+
+        /// <summary>
+        /// ToolTipが表示されている時間[ms]
+        /// </summary>
+        public int ToolTipInterval = int.MaxValue;
+        
+        /// <summary>
+        /// ToolTip本体
+        /// 
+        /// null突っ込むの禁止。
+        /// デフォルトでToolTipExが代入されている。
+        /// </summary>
+        public ToolTipEx ToolTip { get; set; }
+
+        public ToolStripEx() : base()
+        {
+            // 標準のToolTip
+            ShowItemToolTips = false;
+            timer = new Timer();
+            timer.Enabled = false;
+
+            // ToolTipが表示されるまでに要する時間。[ms]
+            timer.Interval = SystemInformation.MouseHoverTime;
+            timer.Tick += new EventHandler(timer_Tick);
+
+            ToolTip = new ToolTipEx();
+        }
+
+        protected override void OnMouseMove(MouseEventArgs mea)
+        {
+            base.OnMouseMove(mea);
+
+            // マウスを留めているところにあるItemの取得
+            var newMouseOverItem = this.GetItemAt(mea.Location);
+            if (mouseOverItem != newMouseOverItem ||
+                (Math.Abs(mouseOverPoint.X - mea.X) > SystemInformation.MouseHoverSize.Width ||
+                (Math.Abs(mouseOverPoint.Y - mea.Y) > SystemInformation.MouseHoverSize.Height))
+                )
+            {
+                mouseOverItem = newMouseOverItem;
+                mouseOverPoint = mea.Location;
+                ToolTip.Hide(this);
+                timer.Stop();
+                timer.Start();
+            }
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+
+            var newMouseOverItem = this.GetItemAt(e.Location);
+            if (newMouseOverItem != null)
+                ToolTip.Hide(this);
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            timer.Stop();
+            ToolTip.Hide(this);
+            mouseOverPoint = new Point(-50, -50);
+            mouseOverItem = null;
+        }
+
+        /// <summary>
+        /// マウスを一定時間留めたのでToolTipを表示する。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+
+            try
+            {
+                var currentMouseOverPoint = this.PointToClient(new Point(
+                    Control.MousePosition.X,
+                    Control.MousePosition.Y + Cursor.Current.Size.Height - Cursor.Current.HotSpot.Y));
+
+                if (mouseOverItem != null && !mouseOverItem.ToolTipText.Empty())
+                {
+                    if ((!(mouseOverItem is ToolStripDropDownButton) && !(mouseOverItem is ToolStripSplitButton)) ||
+                        ((mouseOverItem is ToolStripDropDownButton) && !((ToolStripDropDownButton)mouseOverItem).DropDown.Visible) ||
+                        (((mouseOverItem is ToolStripSplitButton) && !((ToolStripSplitButton)mouseOverItem).DropDown.Visible)))
+                    {
+                        ToolTip.Show(mouseOverItem.ToolTipText, this, currentMouseOverPoint, ToolTipInterval);
+                    }
+                }
+            }
+            catch
+            { }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                timer.Dispose();
+                ToolTip.Dispose();
+            }
+        }
+
+        private Timer timer;
+        private Point mouseOverPoint;
+        private ToolStripItem mouseOverItem = null;
     }
 }
